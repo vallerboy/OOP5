979af48a7964798d691dad0b7f8f8d8f9d3de554
Oskar Polak <biuro@fivarto.pl>
2018-03-27 21:00:12 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Troll troll1 = new Troll(5, 10, 2);
+        Troll troll2 = new Troll(7, 9, 3);
+
+        System.out.println(troll1.def);
+        System.out.println(troll1.attack);
+        System.out.println(troll1.level);
+
+        System.out.println(troll2.attack);
+
+    }
+}
---
+++ src/Troll.java
@@ -0,0 +1,12 @@
+public class Troll {
+    int attack;
+    int def;
+    int level;
+
+
+    Troll(int att, int de, int lvl){
+        attack = att;
+        def = de;
+        level = lvl;
+    }
+}
